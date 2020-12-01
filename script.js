5dfbdfc15066ddff17f2d4d32571646e79d4d067
mohamedfarid <mhmdmedo820@gmail.com>
2020-12-01 21:42:00 +0200

    fix share button event

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded',()=>{
   const shareArea =document.querySelector('.share-area')
   const shareBtn = document.querySelector('.share-area > button')
-  shareBtn.addEventListener("focus",(e)=>{
-    shareArea.classList.add("opened")
+  shareBtn.addEventListener("click",(e)=>{
+    shareArea.classList.toggle("opened")
   })
   shareBtn.addEventListener("focusout",(e)=>{
     shareArea.classList.remove("opened")
